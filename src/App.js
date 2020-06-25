b8628a2bf629400478b7fb6922bbd82c59c66902
João Neves <joao.neves@enecad.com.br>
2020-06-25 20:58:31 -0300

    last commit two test passed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import api from './services/api';
 
 import "./styles.css";
 
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get('repositories').then(response => {
+      setRepositories(response.data);
+    })  
+  }, []);
+
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post('repositories', {
+      title: `Teste ${Date.now()}`,
+      techs: 'React e Node',
+      url: 'http://www.google.com'
+    });
+
+    const singleRepository = response.data;
+
+    setRepositories([...repositories, singleRepository])
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    // const repositoryIndex = repositories.findIndex(
+    //   repo => repo.id === id
+    // );
+
+    // if (repositoryIndex < 0) {
+    //   return null;
+    // }
+
+    // let newList = repositories;
+    // newList.splice(repositoryIndex, 1)
+
+    // setRepositories(newList);
+
+    // console.log(repositories);
+
+    const filteredRepositories = repositories.filter(repository => repository.id !== id);
+
+    setRepositories(filteredRepositories)
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map( repo => (
+          <li key={repo.id}>
+            {repo.title}
+            <button onClick={() => handleRemoveRepository(repo.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
